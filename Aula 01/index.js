ac9e479eefe8e137d90e24f162fa5f8e5c22d7a5
Léo Raiz <leonardoraiz@icloud.com>
2023-02-03 10:48:58 -0300

    Aula 01

---
+++ Aula 01/index.js
@@ -0,0 +1,17 @@
+//Criar a biblioteca express
+const express = require("express");
+
+//armazenar na váriavel app as funções da biblioteca express
+const app = express();
+
+//chamar a função listen para "escutar a porta em que vamos utilizar"
+app.listen(8080);
+
+
+//Criando um rota de teste
+
+app.get('/teste', () => {
+    console.log('Nosso servidor está ativo na porta 8080');
+})
+
+
