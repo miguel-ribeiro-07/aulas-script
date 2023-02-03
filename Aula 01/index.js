66d10c701c48e76066de83a2474522a8eda4bd04
Léo Raiz <leonardoraiz@icloud.com>
2023-02-03 13:10:25 -0300

    Update

--- Aula 01/index.js
+++ Aula 01/index.js
@@ -7,11 +7,13 @@ const app = express();
 //chamar a função listen para "escutar a porta em que vamos utilizar"
 app.listen(8080);
 
+//Criando uma rota de teste
 
-//Criando um rota de teste
+app.get("/teste", () => {
+  console.log("Nosso servidor está ativo na porta 8080");
+});
 
-app.get('/teste', () => {
-    console.log('Nosso servidor está ativo na porta 8080');
-})
-
-
+//Criando uma rota de teste com HTML
+app.get("/testeComHTML", (req, res) => {
+  res.send("<h1>Server ativo</h1>");
+});
