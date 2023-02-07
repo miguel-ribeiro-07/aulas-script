452fa6fe090e6abaafaf92e69d082102087e181e
Léo Raiz <leonardoraiz@icloud.com>
2023-02-07 07:33:00 -0300

    Aula 02

---
+++ Aula_02/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(8080, () => console.log("Servidor ativo na porta 8080"));
+
+app.use(express.json());
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let nomes = ["Lois", "Lana", "Clark", "Bruce"];
+
+//Entrada via JSON
+app.post("/testeGET", (req, res) => {
+  const { name } = req.body;
+  console.log(name);
+});
+
+//Entrada via HTML
+app.post("/testeGETHTML", (req, res) => {
+  const { name } = req.body;
+  console.log(name);
+  nomes.push(name);
+
+  return res.send(`<h1>${[nomes]}</h1>`)
+});
+
+
