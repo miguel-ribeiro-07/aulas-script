4fda7a2dbbbcecf1e373bc4f935b31bdb3c727d7
Léo Raiz <leonardoraiz@icloud.com>
2023-03-01 16:37:36 -0300

    Aula 04

---
+++ Aula_04/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const sql = require('mssql');
+
+const app = express();
+const prisma = new PrismaClient();
+
+const config = {
+  user: 'sa',
+  password: '123456',
+  server: 'localhost',
+  port: 1434,
+  database: 'SCRIPT',
+  trustServerCertificate: true
+};
+
+app.get('/alunos', async (req, res) => {
+  const pool = await sql.connect(config);
+  const result = await pool.request().query('SELECT * FROM aluno');
+  res.json(result.recordset);
+});
+
+app.listen(8080, () => {
+  console.log('Servidor rodando na porta 8080');
+});
